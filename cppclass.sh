4a5fb22e896fbc811ad455a2f1b7ef087f5cd4c6
lspiess <lambert.spiess@gmail.com>
2020-11-19 18:49:22 +0100

    first commit

---
+++ cppclass.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+	echo "class name missing"
+	exit
+fi
+
+# Lowercase the name of the class
+NAME=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+# Uppercase the first letter of the class
+CLASSNAME="${NAME^}"
+# Oppercase the entire name of the class
+UPPERCASENAME=$(echo "$NAME" | tr '[:lower:]' '[:upper:]')
+HEADER="$CLASSNAME.hpp"
+DEFINITIONS="$CLASSNAME.cpp"
+
+touch $HEADER
+echo -en "#ifndef ${UPPERCASENAME}_HPP\n"				>> $HEADER
+echo -en "# define ${UPPERCASENAME}_HPP\n"				>> $HEADER
+echo -en "\n"											>> $HEADER
+echo -en "class\t\t\t\t$CLASSNAME"						>> $HEADER
+echo -en "{\n"											>> $HEADER
+echo -en "\tpublic:\n"									>> $HEADER
+echo -en "\t\t${CLASSNAME}();\n"						>> $HEADER
+echo -en "\t\tvirtual ~${CLASSNAME}();\n"				>> $HEADER
+echo -en "\tprotected:\n"								>> $HEADER
+echo -en "\tprivate:\n"									>> $HEADER
+echo -en "};\n"											>> $HEADER
+echo -en "\n"											>> $HEADER
+echo -en "#endif //${UPPERCASENAME}_HPP\n"				>> $HEADER
+
+touch $DEFINITIONS
+echo -en "#include \"${CLASSNAME}.hpp\"\n"				>> $DEFINITIONS
+echo -en "\n"											>> $DEFINITIONS
+echo -en "${CLASSNAME}::${CLASSNAME}()\n"				>> $DEFINITIONS
+echo -en "{\n"											>> $DEFINITIONS
+echo -en "\t//ctor\n"									>> $DEFINITIONS
+echo -en "}\n"											>> $DEFINITIONS
+echo -en "\n"											>> $DEFINITIONS
+echo -en "${CLASSNAME}::~${CLASSNAME}()\n"				>> $DEFINITIONS
+echo -en "{\n"											>> $DEFINITIONS
+echo -en "\t//dtor\n"									>> $DEFINITIONS
+echo -en "}\n"											>> $DEFINITIONS
